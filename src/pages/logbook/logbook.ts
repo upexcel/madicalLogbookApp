45c5c07b618874fe4e4831b13e0b9342a65ba4cc
upexcel <rahul.excel2011@gmail.com>
2017-10-23 16:54:57 +0530

    implementing logbook

--- src/pages/logbook/logbook.ts
+++ src/pages/logbook/logbook.ts
@@ -1,17 +1,50 @@
-import {Component, OnInit} from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, ModalController } from 'ionic-angular';
 import { AfoListObservable, AngularFireOfflineDatabase } from 'angularfire2-offline/database';
+import { AddOperationPage } from '../add-operation/add-operation';
 
 @Component({
   selector: 'page-logbook',
   templateUrl: 'logbook.html'
 })
 export class LogbookPage implements OnInit {
-  logs: AfoListObservable<any[]>;
-  constructor(afoDatabase: AngularFireOfflineDatabase, public navCtrl: NavController) {
-    this.logs = afoDatabase.list('/logs');
+  logs: any;
+  searchLogText: string = '';
+  apiData: any;
+  constructor(public afoDatabase: AngularFireOfflineDatabase, public navCtrl: NavController, public modalCtrl: ModalController) {
+    afoDatabase.list('/logs').subscribe((res) => {
+      this.logs = res;
+      this.apiData = res;
+    });
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
+  searchLog(ev: any) {
+    this.logs = this.apiData;
+    let val = ev.target.value;
+    if (val && val.trim() != '') {
+      this.logs = this.logs.filter((item) => {
+        return (item['procedure'].toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  logSelected(logData) {
+    console.log(logData)
+  }
+
+  onSearchCancel() {
+    this.searchLogText = '';
+    this.logs = this.apiData;
+  }
+
+  trackLogs(index, data)  {
+    return index;
+  }
+
+  addOperation() {
+    let addOperationModel = this.modalCtrl.create(AddOperationPage);
+    addOperationModel.present();
+  }
 }
