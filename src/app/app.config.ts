f1d2f72b25cd423099d124278b1bd885d0c51522
upexcel <rahul.excel2011@gmail.com>
2017-10-26 16:57:42 +0530

    added speciality dropdown. Added speciality icon in logbook

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,18 @@ export const config = {
         storageBucket: "medical-logbook.appspot.com",
         messagingSenderId: "325280445800"
     },
-    'googleWebClientId': '325280445800-qpj697soklhgeh55ocnnb6e4se647ih6.apps.googleusercontent.com'
+    'googleWebClientId': '325280445800-qpj697soklhgeh55ocnnb6e4se647ih6.apps.googleusercontent.com',
+    'specialityList': [
+        { 'name': 'Cardiothoracic Surgery', 'value': 'cardiothoracic_surgery', 'icon': 'cardiothoracic_surgery.svg'},
+        { 'name': 'ENT (Otolaryngology)', 'value': 'ENT_Otolaryngology', 'icon': 'ENT_Otolaryngology.svg'},
+        { 'name': 'General Surgery', 'value': 'general_surgery', 'icon': 'general_surgery.svg'},
+        { 'name': 'Neurosurgery', 'value': 'Neurosurgery', 'icon': 'Neurosurgery.svg'},
+        { 'name': 'Obstetrics & Gynaecology', 'value': 'obstetrics&gynaecology', 'icon': 'obstetrics&gynaecology.svg'},
+        { 'name': 'Ophthalmology', 'value': 'Ophthalmology', 'icon': 'Ophthalmology.svg'},
+        { 'name': 'Paediatrics', 'value': 'Paediatrics', 'icon': 'Paediatrics.svg'},
+        { 'name': 'Plastic', 'value': 'Plastic', 'icon': 'Plastic.svg'},
+        { 'name': 'Trauma & Orthopaedic', 'value': 'trauma&orthopaedic', 'icon': 'trauma&orthopaedic.svg'},
+        { 'name': 'Urology', 'value': 'Urology', 'icon': 'Urology.svg'},
+        { 'name': 'Vascular', 'value': 'Vascular', 'icon': 'Vascular.svg'},
+    ]
 };
