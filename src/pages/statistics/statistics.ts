5d3290d01fd6f72b5895f512e5816ee8c5328a32
upexcel <rahul.excel2011@gmail.com>
2017-10-31 11:14:07 +0530

    added chart in statistics page

--- src/pages/statistics/statistics.ts
+++ src/pages/statistics/statistics.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ModalController, NavController } from 'ionic-angular';
 import { HomeService } from '../../providers/home/home-service';
-
+import { Chart } from 'chart.js';
+import { config } from '../../app/app.config';
 @Component({
   selector: 'page-statistics',
   templateUrl: 'statistics.html'
@@ -9,12 +10,33 @@ import { HomeService } from '../../providers/home/home-service';
 export class StatisticsPage implements OnInit {
   statisticsData: any;
   statisticsSpinner: boolean = true;
+  @ViewChild('lineCanvas') lineCanvas;
+  lineChart: any;
   constructor(public modalCtrl: ModalController, public navCtrl: NavController, public _homeService: HomeService) { }
 
-  ngOnInit() {
-    this._homeService.getHomePageData().then((statisticsData) => {
+  ngOnInit() { }
+
+  ionViewWillEnter() {
+    this._homeService.getHomePageData().then((statisticsData: any) => {
       this.statisticsSpinner = false;
       this.statisticsData = statisticsData;
+      this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+        type: 'line',
+        data: {
+          labels: statisticsData.chartData.labels,
+          datasets: [
+            {
+              label: "Theatre Time (Last 12 months)",
+              backgroundColor: "rgba(75,192,192,0.4)",
+              borderColor: "rgba(75,192,192,1)",
+              pointBorderColor: "rgba(75,192,192,1)",
+              pointRadius: 1,
+              data: statisticsData.chartData.data
+            }
+          ]
+        },
+        options: config.chartOption
+      });
     });
   }
 
